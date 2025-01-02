dee5dece9cd1329c0d439a5ed06043e8bf8185ff
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2025-01-02 18:49:02 +0900

    ゲーム進行記録

--- src/App.js
+++ src/App.js
@@ -59,6 +59,23 @@ export default function Board() {
   );
 }
 
+export default function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <ol>{}</ol>
+      </div>
+    </div>
+  );
+}
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
